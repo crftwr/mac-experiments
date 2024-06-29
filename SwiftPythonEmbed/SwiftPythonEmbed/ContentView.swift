a7e666aaafceca0b317240737065f989b2c7f5d6
craftware <craftware@gmail.com>
2024-06-29 06:34:58 -0700

    Starting Swift - Python embeding test

---
+++ SwiftPythonEmbed/SwiftPythonEmbed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftPythonEmbed
+//
+//  Created by Shimomura, Tomonori on 2024-06-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftPythonEmbed/SwiftPythonEmbed/SwiftPythonEmbedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPythonEmbedApp.swift
+//  SwiftPythonEmbed
+//
+//  Created by Shimomura, Tomonori on 2024-06-29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPythonEmbedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
