0cf281d901de8d78c5336201737624ca5ac3b82b
craftware <craftware@gmail.com>
2024-06-29 07:26:11 -0700

    Added navigation stack test

--- SwiftUiSandbox/SwiftUiSandbox/ContentView.swift
+++ SwiftUiSandbox/SwiftUiSandbox/ContentView.swift
@@ -13,43 +13,65 @@ struct ContentView: View {
         @State var msg = "いいね"
         @State var input = ""
 
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.orange)
-            
-            Text("Hello, world!")
-                .foregroundColor(.green)
-                .background(.blue)
-            
-            HStack{
-                Text("こんにちは").foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                Text("お元気ですか？").foregroundColor(.red)
+        NavigationStack{
+            VStack {
+                NavigationLink("Next view", destination: SubView())
+
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.orange)
+                
+                Text("Hello, world!")
+                    .foregroundColor(.green)
+                    .background(.blue)
+                
+                HStack{
+                    Text("こんにちは").foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    Text("お元気ですか？").foregroundColor(.red)
+                }
+                
+                Text(msg)
+                Button("Change"){
+                    msg = "最高"
+                }
+                
+                TextField("何かを入力してください:", text:$input)
+                Text(input)
+                
+                List{
+                    Text("Item1")
+                    Text("Item2")
+                    Text("Item3")
+                    Text("Item4")
+                    Text("Item5")
+                }
+                
+                Image("account-icon")
+                    .resizable()
+                    .frame(width:50, height:50)
             }
-            
-            Text(msg)
-            Button("Change"){
-                msg = "最高"
-            }
-            
-            TextField("何かを入力してください:", text:$input)
-            Text(input)
-            
-            List{
-                Text("Item1")
-                Text("Item2")
-                Text("Item3")
-                Text("Item4")
-                Text("Item5")
-            }
-            
-            Image("account-icon")
-                .resizable()
-                .frame(width:50, height:50)
+            .padding()
         }
-        .padding()
     }
 }
+
+struct SubView: View {
+    var body: some View {
+        
+        let cinema = ["Movie1", "Movie2", "Movie3"]
+
+        VStack{
+            VStack{
+                Spacer()
+                ForEach(cinema, id: \.self){
+                    item in Text(item)
+                    Spacer()
+                }
+            }
+        }
+    }
+}
+
 
 #Preview {
     ContentView()
