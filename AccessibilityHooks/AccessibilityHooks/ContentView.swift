82af50eb1e0e1a44e7a2c10ed6c65072d7c8b0cc
crftwr <craftware@gmail.com>
2024-09-15 19:44:14 -0700

    Adding KeyboardHook class, and buttons

--- AccessibilityHooks/AccessibilityHooks/ContentView.swift
+++ AccessibilityHooks/AccessibilityHooks/ContentView.swift
@@ -10,13 +10,28 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Install keyboard hook"){
+                
+                if keyboard_hook != nil {
+                    print("Keyboard hook is already installed.")
+                    return
+                }
+                
+                keyboard_hook = KeyboardHook()
+                keyboard_hook!.Install()
+            }
+            Button("Uninstall keyboard hook"){
+
+                if let existing_keyboard_hook = keyboard_hook {
+                    existing_keyboard_hook.Uninstall()
+                    keyboard_hook = nil
+                }
+            }
         }
         .padding()
     }
+    
+    @State var keyboard_hook: KeyboardHook? = nil
 }
 
 #Preview {
---
+++ AccessibilityHooks/AccessibilityHooks/KeyboardHook.swift
@@ -0,0 +1,22 @@
+//
+//  KeyboardHook.swift
+//  AccessibilityHooks
+//
+//  Created by Tomonori Shimomura on 2024-09-15.
+//
+
+import Foundation
+
+struct KeyboardHook {
+    
+    func Install() {
+        print("KeyboardHook.Install()")
+    }
+    
+    func Uninstall() {
+        print("KeyboardHook.Uninstall()")
+    }
+    
+    var hook_id = 0;
+}
+
