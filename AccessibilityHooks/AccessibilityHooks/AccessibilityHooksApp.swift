b7545fa6e6c993f2d8962859207f3de52a4c8a0c
crftwr <craftware@gmail.com>
2024-09-15 19:12:25 -0700

    Test accessibility & hooks

---
+++ AccessibilityHooks/AccessibilityHooks/AccessibilityHooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityHooksApp.swift
+//  AccessibilityHooks
+//
+//  Created by Tomonori Shimomura on 2024-09-15.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityHooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessibilityHooks/AccessibilityHooks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AccessibilityHooks
+//
+//  Created by Tomonori Shimomura on 2024-09-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
