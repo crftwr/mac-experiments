df0b7359a0b856aef8ca965c1942f9d009bfd1e1
crftwr <craftware@gmail.com>
2024-09-21 11:18:18 -0700

    Added short cut, submenu, and Quit menu

--- MenuBarExtraApp/MenuBarExtraApp/MyMenu.swift
+++ MenuBarExtraApp/MenuBarExtraApp/MyMenu.swift
@@ -11,15 +11,25 @@ struct MyMenu: View {
     var body: some View {
         Button("Test1"){
             print("Test1")
-        }
+        }.keyboardShortcut("1")
         Button("Test2"){
             print("Test2")
         }
         
         Divider()
         
-        Button("Test3"){
-            print("Test3")
+        Menu("Submenu") {
+            Button("Test3"){
+                print("Test3")
+            }
+            Button("Test4"){
+                print("Test4")
+            }
         }
+
+        Button("Quit"){
+            print("Quit")
+            NSApplication.shared.terminate(nil)
+        }.keyboardShortcut("q")
     }
 }
