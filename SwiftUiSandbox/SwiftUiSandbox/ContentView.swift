a7ee20a0e0d9f4b2853b8bd819411f1166e88c65
crftwr <craftware@gmail.com>
2024-09-21 20:26:57 -0700

    Testing TabView

--- SwiftUiSandbox/SwiftUiSandbox/ContentView.swift
+++ SwiftUiSandbox/SwiftUiSandbox/ContentView.swift
@@ -16,40 +16,55 @@ struct ContentView: View {
 
         NavigationStack{
             VStack {
-                NavigationLink("Next view", destination: SubView())
+                TabView {
+                    Tab("Tab1", systemImage: "tray.and.arrow.down.fill") {
+                        NavigationLink("Navigation link", destination: SubView())
 
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.orange)
-                
-                Text("Hello, world!")
-                    .foregroundColor(.green)
-                    .background(.blue)
-                
-                HStack{
-                    Text("こんにちは").foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                    Text("お元気ですか？").foregroundColor(.red)
+                        Image(systemName: "globe")
+                            .imageScale(.large)
+                            .foregroundColor(.orange)
+                        
+                        Text("Hello, world!")
+                            .foregroundColor(.green)
+                            .background(.blue)
+                        
+                        HStack{
+                            Text("こんにちは").foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                            Text("お元気ですか？").foregroundColor(.red)
+                        }
+                        
+                        Text(msg)
+                        Button("Change text message"){
+                            msg = "最高"
+                        }
+                        
+                        TextField("何かを入力してください:", text:$input)
+                        Text(input)
+                        
+                        List{
+                            Text("Item1")
+                            Text("Item2")
+                            Text("Item3")
+                            Text("Item4")
+                            Text("Item5")
+                        }
+                        
+                        Image("account-icon")
+                            .resizable()
+                            .frame(width:50, height:50)
+                    }
+                    .badge(2)
+
+                    Tab("Tab2", systemImage: "tray.and.arrow.up.fill") {
+                        Text("Tab content 2")
+                    }
+
+                    Tab("Tab3", systemImage: "person.crop.circle.fill") {
+                        Text("Tab content 3")
+                    }
+                    .badge("!")
                 }
                 
-                Text(msg)
-                Button("Change"){
-                    msg = "最高"
-                }
-                
-                TextField("何かを入力してください:", text:$input)
-                Text(input)
-                
-                List{
-                    Text("Item1")
-                    Text("Item2")
-                    Text("Item3")
-                    Text("Item4")
-                    Text("Item5")
-                }
-                
-                Image("account-icon")
-                    .resizable()
-                    .frame(width:50, height:50)
             }
             .padding()
         }
