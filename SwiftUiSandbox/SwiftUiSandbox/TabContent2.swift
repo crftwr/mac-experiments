69a48030c0246ffc98e28d9a637071fd54d95548
crftwr <craftware@gmail.com>
2024-09-21 21:46:04 -0700

    Test multi-line text & scroll

--- SwiftUiSandbox/SwiftUiSandbox/TabContent2.swift
+++ SwiftUiSandbox/SwiftUiSandbox/TabContent2.swift
@@ -9,7 +9,34 @@ import SwiftUI
 
 struct TabContent2: View {
     
+    @State var message_lines: [String] = []
+    @State var count = 0
+    let max_lines = 100
+    
+    var message: String {
+        return message_lines.joined()
+    }
+
     var body: some View {
-        Text("Content2")
+        
+        Button("Add a line"){
+            let now = Date()
+            self.count += 1
+            
+            message_lines.append("\(now) \(count)\n")
+            
+            if message_lines.count > max_lines {
+                message_lines.removeFirst( message_lines.count - max_lines )
+            }
+        }
+
+        ScrollView{
+            Text(self.message)
+                .textSelection(.enabled)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .multilineTextAlignment(.leading)
+        }
+        .border(Color.black)
+
     }
 }
