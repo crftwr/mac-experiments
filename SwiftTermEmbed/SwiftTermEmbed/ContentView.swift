772634ab5476bcb47adb4401256cf650edcff6e3
crftwr <craftware@gmail.com>
2024-09-28 22:36:14 -0700

    Starting SwiftTerm embedding experiment

---
+++ SwiftTermEmbed/SwiftTermEmbed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftTermEmbed
+//
+//  Created by Tomonori Shimomura on 2024-09-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftTermEmbed/SwiftTermEmbed/SwiftTermEmbedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTermEmbedApp.swift
+//  SwiftTermEmbed
+//
+//  Created by Tomonori Shimomura on 2024-09-28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTermEmbedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
