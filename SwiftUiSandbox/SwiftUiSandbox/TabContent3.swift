5418ea14542ac76cf0e3268e229d53a6572ee724
crftwr <craftware@gmail.com>
2024-11-29 13:26:13 -0800

    Testing list and filter

--- SwiftUiSandbox/SwiftUiSandbox/TabContent3.swift
+++ SwiftUiSandbox/SwiftUiSandbox/TabContent3.swift
@@ -8,21 +8,34 @@
 import SwiftUI
 
 struct TabContent3: View {
-    
-    @State var message = ""
-    @State var count = 0
+    let names = ["Holly", "Josh", "Rhonda", "Ted"]
+    @State private var searchText = ""
 
     var body: some View {
         
-        Button("Add a line"){
-            let now = Date()
-            self.count += 1
-            
-            let new_line = "\(now) \(count)\n"
-            message += new_line
+        TextField("Search", text: $searchText)
+        
+        List {
+            ForEach(searchResults, id: \.self) { name in
+                Text(name)
+            }
         }
+    }
 
-        TextEditor(text: $message)
-            .border(Color.black)
+    var searchResults: [AttributedString] {
+        if searchText.isEmpty {
+            let names3 = names.map {
+                try! AttributedString(markdown: $0.replacingOccurrences(of: searchText, with: String(format: "**%@**", searchText) ))
+            }
+            return names3
+
+        } else {
+            let names2 = names.filter { $0.contains(searchText) }
+            let names3 = names2.map {
+                try! AttributedString(markdown: $0.replacingOccurrences(of: searchText, with: String(format: "**%@**", searchText) ))
+            }
+            return names3
+        }
     }
 }
+
