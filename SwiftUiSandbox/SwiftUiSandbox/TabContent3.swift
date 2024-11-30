3ac9a8ab553c0c120e4c40a0df24f7619336ac94
crftwr <craftware@gmail.com>
2024-11-29 17:33:13 -0800

    Always focus on text field, and control list index remotely

--- SwiftUiSandbox/SwiftUiSandbox/TabContent3.swift
+++ SwiftUiSandbox/SwiftUiSandbox/TabContent3.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 struct TabContent3: View {
-    let names = ["Holly", "Josh", "Rhonda", "Ted"]
+    
+    enum Field: Hashable {
+        case textField
+        //case list
+    }
+    
+    @FocusState private var focusedField: Field?
+    @State private var focusedListItem: Int = 0
+
+    
+    let names = ["Holly", "Josh", "Rhonda", "Ted", "Item001", "Item002", "Item003", "Item004"]
     @State private var searchText = ""
+
+    @State private var scrollPosition = ScrollPosition(edge: .top)
 
     var body: some View {
         
@@ -18,21 +30,71 @@ struct TabContent3: View {
                 .imageScale(.medium)
                 .frame(width: 16, height: 8, alignment: .center)
             TextField("Search", text: $searchText)
+                .focused($focusedField, equals: .textField)
+                .onChange(of: searchText) { oldValue, newValue in
+                    focusedListItem = 0
+                }
         }
-            
-        List {
-            ForEach(searchResults, id: \.self) { name in
-                HStack {
-                    Text("📋")
-                        .font(.system(size: 8))
-                        .frame(width: 12)
-                        .foregroundStyle(.opacity(0.5))
+        .onAppear() {
+            focusedField = .textField
+        }
+        .onKeyPress(.upArrow) {
 
-                    Text(name)
+            let searchResults = self.searchResults
+
+            focusedListItem = max(focusedListItem-1, 0)
+            if focusedListItem < searchResults.count {
+                scrollPosition.scrollTo(id: searchResults[focusedListItem], anchor: .top)
+            }
+            else {
+                scrollPosition.scrollTo(edge: .top)
+            }
+
+            return .handled
+        }
+        .onKeyPress(.downArrow) {
+
+            let searchResults = self.searchResults
+
+            focusedListItem = max(min(focusedListItem+1, searchResults.count-1), 0)
+            if focusedListItem < searchResults.count {
+                scrollPosition.scrollTo(id: searchResults[focusedListItem], anchor: .bottom)
+            }
+            else {
+                scrollPosition.scrollTo(edge: .top)
+            }
+
+            return .handled
+        }
+
+        ScrollView {
+            LazyVStack {
+                let searchResults = self.searchResults
+                
+                ForEach(Array(searchResults.enumerated()), id: \.element) { index, name in
+                    
+                    let focused = focusedListItem == index
+                    
+                    HStack {
+                        Text("📋")
+                            .font(.system(size: 8))
+                            .frame(width: 12)
+                            .foregroundStyle(.opacity(0.5))
+
+                        Text(name)
+                        
+                        Spacer()
+                    }
+                    .id(name)
+                    .background(focused ? Color.blue.opacity(0.3) : Color.clear)
                 }
             }
+            .padding(.all, 8)
         }
-        .listStyle(.plain)
+        .background(Color.white)
+//        .focusable()
+//        .focused($focusedField, equals: .list)
+        .scrollPosition($scrollPosition)
     }
 
     var searchResults: [AttributedString] {
